d7406407640442a64618739cc4c0837d9ee0803b
Kevin Bombino <kevin@bombino.org>
2016-04-11 21:26:39 -0700

    Fix left menu

--- js/vae_book.js
+++ js/vae_book.js
@@ -27,13 +27,54 @@ $(function() {
     $(this).attr("href", img.attr("src"));
   }).lightBox();
 
-  var activeChild = $('.child-link.active').closest('li');
-  if (activeChild.length) {
-    var prevLink = activeChild.prev('.child-item');
-    var nextLink = activeChild.next('.child-item');
+  $("#content .btm a").each(function() {
+    var a = $(this);
+    if (window.location.pathname == a.attr("href")) {
+      a.parent().addClass("active");
+    }
+  });
+  if ($("#content .btm li.active").length > 1) {
+    $("#content .btm li.parent").removeClass("active");
+  }
+  var parentHref;
+  if ($("#content .btm li.child.active").length > 0) {
+    var prev = $("#content .btm li.active").prevUntil("li.parent").prev();
+    if (!prev.length > 0) prev = $("#content .btm li.active").prev();
 
-    if (prevLink.length) $('.pagenav .prev').attr('href', prevLink.find('a').attr('href')).show().find('span').text(prevLink.find('a').text().substr(2));
-    if (nextLink.length) $('.pagenav .next').attr('href', nextLink.find('a').attr('href')).show().find('span').text(nextLink.find('a').text().substr(2));
+    parentHref = prev.find("a").attr("href");
+  } else if ($("#content .btm li.parent.active").length > 0) {
+    parentHref = $("#content .btm li.active a").attr("href");
+  }
+  console.log("parent Href is " + parentHref);
+  var inParent = false;
+  $("#content .btm li").each(function() {
+    var a = $(this);
+    if (a.hasClass("parent")) {
+      if (a.find("a").attr("href") == parentHref) {
+        console.log("Found parentHref");
+        if (!a.hasClass("active")) {
+        console.log("adding Up");
+          a.addClass("up");
+        }
+        inParent = true;
+      } else {
+        console.log("not inparent");
+        inParent = false;
+      }
+    } else {
+      if (inParent) {
+        a.show();
+      }
+    }
+  });
+
+  var activePage = $('#content .btm li.active');
+  if (activePage.length) {
+    var prevLink = activePage.prev('li.child');
+    var nextLink = activePage.next('li');
+
+    if (prevLink.length) $('.pagenav .prev').attr('href', prevLink.find('a').attr('href')).show().find('span').text(prevLink.find('a').text());
+    if (nextLink.length) $('.pagenav .next').attr('href', nextLink.find('a').attr('href')).show().find('span').text(nextLink.find('a').text());
 
     $('.pagenav').show();
   }
